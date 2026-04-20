9f80e23c44a772f5e3a7ad63a969e1ed968bf516
Eyrown <aironegamil123@gmail.com>
2026-04-20 14:11:50 +0800

    feat: added sitemap for SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://airone-portfolio.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://airone-portfolio.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://airone-portfolio.vercel.app/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://airone-portfolio.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
